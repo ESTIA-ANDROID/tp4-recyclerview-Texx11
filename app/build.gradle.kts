d0e89c87104990fe5e29aa407357c4e9245a52a7
leotexx16 <leo.texier@etu.estia.fr>
2025-01-29 11:45:48 +0100

    Fixes ActivityTestRule to ActivityScenarioRule

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,12 +50,17 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation (libs.glide)
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation(libs.androidx.espresso.contrib)
+    implementation(libs.androidx.junit.ktx)
+    annotationProcessor (libs.compiler)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.contrib)
+    androidTestImplementation(libs.androidx.rules)
+
     //implementation("com.github.jinatonic:confetti:1.0.0")
 
 }
--- app/src/androidTest/java/com/openclassrooms/magicgithub/UserListInstrumentedTest.kt
+++ app/src/androidTest/java/com/openclassrooms/magicgithub/UserListInstrumentedTest.kt
@@ -5,6 +5,7 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
@@ -27,7 +28,8 @@ import org.junit.runner.RunWith
 class UserListInstrumentedTest {
     @Rule
     @JvmField
-    val mActivityRule = ActivityTestRule(ListUserActivity::class.java)
+    //val mActivityRule = ActivityTestRule(ListUserActivity::class.java)
+    val mActivityRule = ActivityScenarioRule(ListUserActivity::class.java)
 
     private var currentUsersSize = -1
 
